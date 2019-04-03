49bbc0dc0f4f857b13ca89ec060620c6b8ed4a60
Ben Krikler <bek07@ic.ac.uk>
2019-04-03 14:11:37 +0200

    Extend tests

--- tests/test_executor.py
+++ tests/test_executor.py
@@ -1,3 +1,4 @@
+import os
 from wyriwyd.executor import ShellExecutor
 
 
@@ -5,10 +6,15 @@ def test_executor(tmpdir):
     cmds = ["export MY_VARIABLE='hello world'",
             "cd {}".format(str(tmpdir)),
             "pwd",
-            "echo MY_VARIABLE = $MY_VARIABLE"]
+            "echo MY_VARIABLE = $MY_VARIABLE",
+            "pushd ../ \n pwd \n popd"]
 
     outputs = []
     with ShellExecutor() as executor:
         for command in cmds:
             outputs.append(executor.run_command(command))
-    assert outputs == [[], [], [str(tmpdir)], ["MY_VARIABLE = hello world"]]
+    assert outputs[:3] == [[], [], [str(tmpdir)]]
+    assert outputs[3] == ["MY_VARIABLE = hello world"]
+    cmd_out = "{0} {1}:{0}:{1}"
+    cmd_out = cmd_out.format(os.path.dirname(tmpdir), str(tmpdir))
+    assert outputs[4] == cmd_out.split(":")
