cb4e9de9499940f63a71c0f6cc37071f31c3af9f
Ilektra Christidi <ilektra.christidi@ucl.ac.uk>
2019-04-03 14:04:16 +0100

    Check for different OS's.

--- wizard.sh
+++ wizard.sh
@@ -3,8 +3,18 @@ read -p "Output file name (.md): " outfile
 rm -f "$outfile"
 echo -e "Start typing your commands. When done, press enter.
 File $outfile containing the session will be genarated."
+mysys="$(uname -s)"
+tmpfile="/tmp/mytemp"
 while read -p "$ " command && test -n "$command" ; do
-    output="$(script -c "$command" -q --)"
+    if [ "$mysys" = "Linux" ]; then
+        output="$(script -c "$command" -q --)"
+    elif [ "$mysys" = "Darwin" ]; then
+        output="$(script "$tmpfile" "$command" -q ; cat "$tmpfile")"
+        rm "$tmpfile"
+    else
+        echo "Unknown system"
+        return 1
+    fi
     echo -e "$output\n"
     if [ -n "$output" ]; then
         echo -e "\`\`\`bash\n${command}\n\`\`\`\n\n\`\`\`\n$output\n\`\`\`\n" >> "$outfile"
