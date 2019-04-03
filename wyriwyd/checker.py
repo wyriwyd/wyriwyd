816fc890941f2bee8c19d1996eee85e0a90b2145
Ben Krikler <bek07@ic.ac.uk>
2019-04-03 14:55:01 +0200

    Clean up checker

--- wyriwyd/checker.py
+++ wyriwyd/checker.py
@@ -18,6 +18,7 @@ class OutputDoesntMatchExpectation(Exception):
 def check_commands(commands, no_expected_means_empty_out=True):
     with ShellExecutor() as executor:
         for command, expected in commands:
+            print("Running command", command)
             output = executor.run_command(command)
             output = "\n".join(output)
             if expected is None and no_expected_means_empty_out and output:
@@ -29,10 +30,3 @@ def check_commands(commands, no_expected_means_empty_out=True):
                 msg = f"Output not matching expectationg for cmd '''{command}'''"
                 logger.error(msg + "\n Expected:\n%s\nReceived:\n%s", expected, output)
                 raise OutputDoesntMatchExpectation(msg)
-
-
-
-if __name__ == "__main__":
-    from .parser import parse_file
-    commands = parse_file("examples/sample.md")
-    check_commands(commands)
