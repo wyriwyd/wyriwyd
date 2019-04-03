66d6ed6b7345b9805260092e0ec121619c46c50d
Ilektra Christidi <ilektra.christidi@ucl.ac.uk>
2019-04-03 13:43:25 +0100

    Avoid generating - file, prettyfying of md file.

--- wizard.sh
+++ wizard.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-rm -f wizard.out
+read -p "Output file name (.md): " outfile
+rm -f "$outfile"
 echo -e "Start typing your commands. When done, press enter.
-File wizard.out containing the session will be genarated."
+File $outfile containing the session will be genarated."
 while read -p "$ " command && test -n "$command" ; do
-    output="$(script -c "$command" -q -)"
+    output="$(script -c "$command" -q --)"
     echo -e "$output\n"
-    echo -e "\`\`\`bash\n$ ${command}\n\`\`\`\n\n\`\`\`\n$output\n\`\`\`\n" >> wizard.out
+    if [ -n "$output" ]; then
+        echo -e "\`\`\`bash\n${command}\n\`\`\`\n\n\`\`\`\n$output\n\`\`\`\n" >> "$outfile"
+    else
+        echo -e "\`\`\`bash\n${command}\n\`\`\`\n" >> "$outfile"
+    fi
 done
-rm -- -
