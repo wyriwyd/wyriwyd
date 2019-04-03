4c407fbb1cfdf642de408a18033f845f7ab04959
Ilektra Christidi <ilektra.christidi@ucl.ac.uk>
2019-04-03 12:23:44 +0100

    Add markdown ticks, get rid of output for last empty command.

--- wizard.sh
+++ wizard.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-command="ls"
 rm -f wizard.out
 echo -e "Start typing your commands. When done, press enter.
 File wizard.out containing the session will be genarated."
-while [ -n "$command" ] ; do
-    read -p "$ " command
-    #    foo="$(script -c "$command" -q -)"
-    foo="$(eval "$command")"
-    echo -e "$foo\n"
-    echo -e "$ ${command}\n$foo\n" >> wizard.out
+while read -p "$ " command && test -n "$command" ; do
+    output="$(script -c "$command" -q -)"
+    echo -e "$output\n"
+    echo -e "\`\`\`bash\n$ ${command}\n\`\`\`\n\n\`\`\`\n$output\n\`\`\`\n" >> wizard.out
 done
+rm -- -
