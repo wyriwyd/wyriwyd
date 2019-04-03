e690afaac4b9e1216258f50ec1b59e8db8f622d5
Ben Krikler <bek07@ic.ac.uk>
2019-04-03 15:06:20 +0200

    Finish command

--- wyriwyd/__main__.py
+++ wyriwyd/__main__.py
@@ -9,6 +9,8 @@ def prepare_parser():
     from argparse import ArgumentParser
     parser = ArgumentParser(description=__doc__)
     parser.add_argument("infile", help="The path to the input markdown file")
+    parser.add_argument("-r", "--raise-error", default=False, action="store_true",
+                        help="Raise an exception at the first error")
     parser.add_argument("-e", "--empty", help="""If the Markdown file contains
 commands without a following output, and the command when runs
 produces output, treat this as an error""")
@@ -18,5 +20,13 @@ produces output, treat this as an error""")
 def main(args=None):
     args = prepare_parser().parse_args(args=args)
     commands = parse_file(args.infile)
-    print(commands)
-    check_commands(commands, no_expected_means_empty_out=args.empty)
+    errors = check_commands(commands,
+                            no_expected_means_empty_out=args.empty,
+                            raise_error=args.raise_error)
+    if not errors:
+        print("Everything looks ok!!")
+        return 0
+
+    for error in errors:
+        print("Error:", error)
+    return 1
--- wyriwyd/checker.py
+++ wyriwyd/checker.py
@@ -15,18 +15,25 @@ class OutputDoesntMatchExpectation(Exception):
 #     pass
 
 
-def check_commands(commands, no_expected_means_empty_out=True):
+def check_commands(commands, no_expected_means_empty_out=True, raise_error=True):
+    errors = []
     with ShellExecutor() as executor:
         for command, expected in commands:
-            print("Running command", command)
+            #print("Running command", command)
             output = executor.run_command(command)
             output = "\n".join(output)
             if expected is None and no_expected_means_empty_out and output:
                 msg = f"Unexpected output for cmd '''{command}'''"
-                raise OutputWasntExpected(msg)
+                if raise_error:
+                    raise OutputWasntExpected(msg)
+                errors.append(msg)
             if expected is None:
                 continue
             if output != expected:
                 msg = f"Output not matching expectationg for cmd '''{command}'''"
-                logger.error(msg + "\n Expected:\n%s\nReceived:\n%s", expected, output)
-                raise OutputDoesntMatchExpectation(msg)
+                full = msg + f"\n\nExpected:\n{expected}\n\nReceived:\n{output}"
+                if raise_error:
+                    logger.error(full)
+                    raise OutputDoesntMatchExpectation(msg)
+                errors.append(full)
+    return errors
